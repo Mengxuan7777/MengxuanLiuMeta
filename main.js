3cd431ef86b9f7e3573cac2fe60935020bccf4c4
Liu <mengxuan.liu@aecom.com>
2023-10-05 19:02:06 -0400

    20231005-5cardslayout

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const btnToBacks = document.querySelectorAll(".right");
 const btnToFronts = document.querySelectorAll(".left");
 
 function flipToBack(event) {
-    const cardId = event.target.getAttribute('data-cardid');
+    const cardId = event.currentTarget.getAttribute('data-arrowid');
     const cardFront = document.getElementById(`cardFront${cardId}`);
     const cardBack = document.getElementById(`cardBack${cardId}`);
     cardFront.classList.toggle('flipped');
@@ -11,7 +11,7 @@ function flipToBack(event) {
 }
 
 function flipToFront(event) {
-    const cardId = event.target.getAttribute('data-cardid');
+    const cardId = event.currentTarget.getAttribute('data-arrowid');
     const cardFront = document.getElementById(`cardFront${cardId}`);
     const cardBack = document.getElementById(`cardBack${cardId}`);
     cardFront.classList.toggle('flipped');
@@ -28,30 +28,57 @@ function flipToFront(event) {
     })
 
 // Open frontModals
-const cardFronts = document.querySelectorAll('.cardFront');
-const frontModals = document.querySelectorAll('.frontModal');
+const blogs = document.querySelectorAll('.blog');
+const blogModals = document.querySelectorAll('.blogModal');
 const closeBtns =  document.querySelectorAll('.close');
 
-function modalOpen(event) {
-    const modalId = event.target.getAttribute('data-cardfrontid');
+function blogModalOpen(event) {
+    const modalId = event.currentTarget.getAttribute('data-blogid');
     console.log(modalId);
-    const frontModal = document.getElementById(`frontModal${modalId}`);
-    console.log('modal here')
-    frontModal.style.display = 'block';
+    const blogModal = document.getElementById(`blogModal${modalId}`);
+    console.log('modal here');
+    blogModal.style.display = 'block';
 }
 
-cardFronts.forEach((cardFront) => {
-    cardFront.addEventListener('click', modalOpen);
+blogs.forEach((blog) => {
+    blog.addEventListener('click', blogModalOpen);
 });
 
-function modalClose(event) {
+function blogModalClose(event) {
     const modalId = event.target.getAttribute('data-closeBtnid');
     console.log(modalId);
-    const frontModal = document.getElementById(`frontModal${modalId}`);
-    frontModal.style.display = 'none';
+    const blogModal = document.getElementById(`blogModal${modalId}`);
+    blogModal.style.display = 'none';
 }
 
 closeBtns.forEach((closeBtn) => {
-    closeBtn.addEventListener('click', modalClose);
+    closeBtn.addEventListener('click', blogModalClose);
 });
 
+// Open backModals
+const projects = document.querySelectorAll('.project');
+const projectModals = document.querySelectorAll('.projectModal');
+// const closeBtns =  document.querySelectorAll('.close');
+
+function projectModalOpen(event) {
+    const modalId = event.currentTarget.getAttribute('data-projectid');
+    console.log(modalId);
+    const projectModal = document.getElementById(`projectModal${modalId}`);
+    console.log('modal here');
+    projectModal.style.display = 'block';
+}
+
+projects.forEach((project) => {
+    project.addEventListener('click', projectModalOpen);
+});
+
+function projectModalClose(event) {
+    const modalId = event.target.getAttribute('data-closeBtnid');
+    console.log(modalId);
+    const projectModal = document.getElementById(`projectModal${modalId}`);
+    projectModal.style.display = 'none';
+}
+
+closeBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener('click', projectModalClose);
+});
